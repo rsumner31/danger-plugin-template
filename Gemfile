da09b24511e6fa3e70b860daaa60590b8f794cf9
Robert Sumner Jr <rsumner31@gmail.com>
2018-03-08 19:51:17 -0600

    PLUGIN.gemspec.template

--- Gemfile
+++ Gemfile
@@ -2,12 +2,3 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in ${PLUGIN_NAME}.gemspec
 gemspec
-
-group :development do
-  gem 'cocoapods'
-
-  gem 'mocha'
-  gem 'bacon'
-  gem 'mocha-on-bacon'
-  gem 'prettybacon'
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,50 +1,65 @@
-require 'pathname'
-ROOT = Pathname.new(File.expand_path('../../', __FILE__))
-$:.unshift((ROOT + 'lib').to_s)
-$:.unshift((ROOT + 'spec').to_s)
+require "pathname"
+ROOT = Pathname.new(File.expand_path("../../", __FILE__))
+$:.unshift((ROOT + "lib").to_s)
+$:.unshift((ROOT + "spec").to_s)
 
-require 'bundler/setup'
-require 'bacon'
-require 'mocha-on-bacon'
-require 'pretty_bacon'
-require 'pathname'
-require 'cocoapods'
+require "bundler/setup"
+require "pry"
 
-Mocha::Configuration.prevent(:stubbing_non_existent_method)
+require "rspec"
+require "danger"
 
-require 'cocoapods_plugin'
-
-#-----------------------------------------------------------------------------#
-
-module Pod
-
-  # Disable the wrapping so the output is deterministic in the tests.
-  #
-  UI.disable_wrap = true
-
-  # Redirects the messages to an internal store.
-  #
-  module UI
-    @output = ''
-    @warnings = ''
-
-    class << self
-      attr_accessor :output
-      attr_accessor :warnings
-
-      def puts(message = '')
-        @output << "#{message}\n"
-      end
-
-      def warn(message = '', actions = [])
-        @warnings << "#{message}\n"
-      end
-
-      def print(message)
-        @output << message
-      end
-    end
-  end
+if `git remote -v` == ''
+  puts "You cannot run tests without setting a local git remote on this repo"
+  puts "It's a weird side-effect of Danger's internals."
+  exit(0)
 end
 
-#-----------------------------------------------------------------------------#
+# Use coloured output, it's the best.
+RSpec.configure do |config|
+  config.filter_gems_from_backtrace "bundler"
+  config.color = true
+  config.tty = true
+end
+
+require "danger_plugin"
+
+# These functions are a subset of https://github.com/danger/danger/blob/master/spec/spec_helper.rb
+# If you are expanding these files, see if it's already been done ^.
+
+# A silent version of the user interface,
+# it comes with an extra function `.string` which will
+# strip all ANSI colours from the string.
+
+# rubocop:disable Lint/NestedMethodDefinition
+def testing_ui
+  @output = StringIO.new
+  def @output.winsize
+    [20, 9999]
+  end
+
+  cork = Cork::Board.new(out: @output)
+  def cork.string
+    out.string.gsub(/\e\[([;\d]+)?m/, "")
+  end
+  cork
+end
+# rubocop:enable Lint/NestedMethodDefinition
+
+# Example environment (ENV) that would come from
+# running a PR on TravisCI
+def testing_env
+  {
+    "HAS_JOSH_K_SEAL_OF_APPROVAL" => "true",
+    "TRAVIS_PULL_REQUEST" => "800",
+    "TRAVIS_REPO_SLUG" => "artsy/eigen",
+    "TRAVIS_COMMIT_RANGE" => "759adcbd0d8f...13c4dc8bb61d",
+    "DANGER_GITHUB_API_TOKEN" => "123sbdq54erfsd3422gdfio"
+  }
+end
+
+# A stubbed out Dangerfile for use in tests
+def testing_dangerfile
+  env = Danger::EnvironmentManager.new(testing_env)
+  Danger::Dangerfile.new(env, testing_ui)
+end
